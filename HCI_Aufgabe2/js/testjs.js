f2646386d9bb431405b06a2b19ee87211730b9a6
JeanNessen <jean.luc.nessen@gmail.com>
2018-12-02 21:44:19 +0100

    erste tests für custom elements, ohne großen erfolg

---
+++ HCI_Aufgabe2/js/testjs.js
@@ -0,0 +1,20 @@
+(function() {
+  class MyTitle extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <style>
+          h1 {
+            font-size: 2.5rem;
+            color: hotpink;
+            font-family: monospace;
+            text-align: center;
+            text-decoration: pink solid underline;
+            text-decoration-skip: ink;
+          }
+        </style>
+        <h1>Hello Alligator!</h1>
+      `;
+    }
+  }
+      window.customElements.define('my-title', MyTitle);
+})();
