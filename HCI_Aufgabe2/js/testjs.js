2301a7f8ce6c62ce34c02618f82659726157af3f
JeanNessen <jean.luc.nessen@gmail.com>
2018-12-05 01:06:18 +0100

    Sweet vapowave colors, new logo, new pics for books.

--- HCI_Aufgabe2/js/testjs.js
+++ HCI_Aufgabe2/js/testjs.js
@@ -1,20 +1,35 @@
-(function() {
+
+ (function() {
+  const template = document.createElement('template');
+
+  template.innerHTML = `
+      <style>
+        h1 {
+          font-size: 2.5rem;
+          color: hotpink;
+          font-family: monospace;
+          text-align: center;
+          text-decoration: pink solid underline;
+          text-decoration-skip: ink;
+        }
+      </style>
+      <h1 id="frage_1">Frage 1</h1>
+        <h1 id="frage_2" style="display:none">Frage 2</h1>
+        <button id="ryo_button" onclick="document.getElementById('frage_2').style.display='block'">"ryo"</button>
+
+
+  `;
+
   class MyTitle extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `
-        <style>
-          h1 {
-            font-size: 2.5rem;
-            color: hotpink;
-            font-family: monospace;
-            text-align: center;
-            text-decoration: pink solid underline;
-            text-decoration-skip: ink;
-          }
-        </style>
-        <h1>Hello Alligator!</h1>
-      `;
+    constructor() {
+      super();
+
+      this.attachShadow({ mode: 'open' });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+
     }
   }
-      window.customElements.define('my-title', MyTitle);
+
+  window.customElements.define('my-title', MyTitle);
 })();
+
